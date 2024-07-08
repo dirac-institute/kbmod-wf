9aed79dedc8e119c643eff7d1332f3eb082dd02a
drewoldag <47493171+drewoldag@users.noreply.github.com>
2024-07-08 14:46:30 -0700

    Cleaning up logging, adding new tasks directory where the task logic will live.

--- src/kbmod_wf/__init__.py
+++ src/kbmod_wf/__init__.py
@@ -1,4 +1,1 @@
-from .configurations.dev_configuration import dev_config
 from .workflow import workflow_runner
-from .configurations.klone_configuration import klone_config
-from .utilities.logger_utilities import configure_logger
--- src/kbmod_wf/workflow.py
+++ src/kbmod_wf/workflow.py
@@ -1,6 +1,5 @@
 import os
 import parsl
-from datetime import datetime
 from parsl import python_app, File
 import parsl.executors
 
@@ -8,49 +7,50 @@ from kbmod_wf.configurations import *
 
 
 @python_app(executors=["local_dev_testing"])
-def uri_to_ic(inputs=[], outputs=[], logger=None):
+def uri_to_ic(inputs=[], outputs=[], logging_file=None):
     from kbmod_wf.utilities.logger_utilities import configure_logger
+    from kbmod_wf.tasks.uri_to_ic import uri_to_ic
 
-    logger = configure_logger("task:uri_to_ic", inputs[-1].filepath)
+    logger = configure_logger("task.uri_to_ic", logging_file.filepath)
 
     logger.info("Starting uri_to_ic")
+    output = uri_to_ic(logger=logger)
     logger.warning("You're the cool one.")
-    return 42
+    return output
 
 
 @python_app(executors=["local_dev_testing"])
-def part2(inputs=[], outputs=[], logger=None):
+def reproject_ic(inputs=[], outputs=[], logging_file=None):
     from kbmod_wf.utilities.logger_utilities import configure_logger
+    from kbmod_wf.tasks.reproject_ic import reproject_ic
 
-    logger = configure_logger("task:part2", inputs[-1].filepath)
+    logger = configure_logger("task.reproject_ic", logging_file.filepath)
 
-    logger.info("Starting part2")
-    return 43
+    logger.info("Starting reproject_ic")
+    output = reproject_ic(logger=logger)
+    logger.warning("This is a slow step.")
+    return output
 
 
 def workflow_runner():
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    logging_file = File(os.path.join(os.getcwd(), f"kbmod_wf_{timestamp}.log"))
+    with parsl.load(dev_config()) as dfk:
+        logging_file = File(os.path.join(dfk.run_dir, "parsl.log"))
 
-    logger = parsl.set_file_logger(logging_file.filepath)
-
-    with parsl.load(dev_config()):
         uri_list = File(os.path.join(os.getcwd(), "uri_list.txt"))
-
         uri_to_ic_future = uri_to_ic(
-            inputs=[uri_list, logging_file],
-            outputs=[File(os.path.join(os.getcwd(), "ic.ecsv")), logging_file],
+            inputs=[uri_list],
+            outputs=[File(os.path.join(os.getcwd(), "ic.ecsv"))],
+            logging_file=logging_file,
         )
 
-        part2_future = part2(
-            inputs=[logging_file],
-            outputs=[logging_file],
+        reproject_ic_future = reproject_ic(
+            inputs=[],
+            outputs=[],
+            logging_file=logging_file,
         )
-
-        logger.warning("You are here")
 
         print(uri_to_ic_future.result())
-        print(part2_future.result())
+        print(reproject_ic_future.result())
 
     parsl.clear()
 
